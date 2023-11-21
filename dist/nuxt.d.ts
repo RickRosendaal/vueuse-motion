a1354717f441acec029470ee2a02db82fbe837cc
Rick Rosendaal <rickrosendaal@hotmail.com>
2023-11-21 14:44:57 +0100

    Build

---
+++ dist/nuxt.d.ts
@@ -0,0 +1,10 @@
+import * as _nuxt_schema from '@nuxt/schema';
+import { Y as ModuleOptions$1 } from './nuxt-22219810.js';
+import 'vue';
+import '@vueuse/core';
+
+interface ModuleOptions extends ModuleOptions$1<string> {
+}
+declare const _default: _nuxt_schema.NuxtModule<ModuleOptions>;
+
+export { ModuleOptions, _default as default };
---
+++ dist/runtime/templates/motion.mjs
@@ -0,0 +1,8 @@
+import { MotionPlugin } from "@vueuse/motion";
+import { defineNuxtPlugin, useRuntimeConfig } from "#app";
+export default defineNuxtPlugin(
+  (nuxtApp) => {
+    const config = useRuntimeConfig();
+    nuxtApp.vueApp.use(MotionPlugin, config.public.motion);
+  }
+);
